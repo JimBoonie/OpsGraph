73b166404dfac82d5195450f920f13ebfeb192a4
MasonMcGough <mcgough.mason@gmail.com>
2018-08-13 13:28:28 -0400

    Add argparse to specify port when run.py is invoked;

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 from app import app
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', type=int, default=5000, 
+                        help='Port to access application')
+    args = parser.parse_args()
+
+    app.run(debug=True, port=args.port)
